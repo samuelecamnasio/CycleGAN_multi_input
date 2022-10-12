7da62bd5a4065ff83d31a52f5ba220d414e2dcc0
Samuele Camnasio <samuele.camnasio@mail.polimi.it>
2022-10-12 17:29:48 +0200

    Update base_dataset.py

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -11,29 +11,67 @@ class BaseDataset(data.Dataset):
 
     def initialize(self, opt):
         pass
-
-def get_transform(opt):
+    
+#to solve the warning try T.InterpolationMode.BICUBIC instead of Image.BICUBIC
+def get_transform_A(opt,img, flip, i,j,h,w, params=None,  grayscale=False, method=transforms.InterpolationMode.BICUBIC, convert=True):
     transform_list = []
-    if opt.resize_or_crop == 'resize_and_crop':
+    if grayscale:
+        img=transforms.functional.to_grayscale(img,1)
+    if 'resize' in opt.preprocess:
         osize = [opt.loadSize, opt.loadSize]
-        transform_list.append(transforms.Scale(osize, Image.BICUBIC))
-        transform_list.append(transforms.RandomCrop(opt.fineSize))
-    elif opt.resize_or_crop == 'crop':
-        transform_list.append(transforms.RandomCrop(opt.fineSize))
-    elif opt.resize_or_crop == 'scale_width':
-        transform_list.append(transforms.Lambda(
-            lambda img: __scale_width(img, opt.fineSize)))
-    elif opt.resize_or_crop == 'scale_width_and_crop':
-        transform_list.append(transforms.Lambda(
-            lambda img: __scale_width(img, opt.loadSize)))
-        transform_list.append(transforms.RandomCrop(opt.fineSize))
-
+        img=transforms.functional.resize(img, osize, method)
+        
+    if 'crop' in opt.preprocess:
+        if params is None:
+            img=transforms.functional.crop(img, i,j,h,w)
+    
     if opt.isTrain and not opt.no_flip:
-        transform_list.append(transforms.RandomHorizontalFlip())
+        if params is None:
+            if flip> 0.75:
+                img=transforms.functional.hflip(img)
+                
+    if convert:
+        img=transforms.functional.to_tensor(img)
+        if grayscale:
+            img=transforms.functional.normalize(img, (0.5,), (0.5,))
+        else:
+            img=transforms.functional.normalize(img, (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 
-    transform_list += [transforms.ToTensor(),
-                       transforms.Normalize((0.5, 0.5, 0.5),
-                                            (0.5, 0.5, 0.5))]
+    return img
+
+def get_transform_B(opt, params=None,  grayscale=False, method=transforms.InterpolationMode.BICUBIC, convert=True):
+    transform_list = []
+    if grayscale:
+        transform_list.append(transforms.Grayscale(1))
+    if 'resize' in opt.preprocess:
+        osize = [opt.loadSize, opt.loadSize]
+        transform_list.append(transforms.Resize(osize, method))
+    elif 'scale_width' in opt.preprocess:
+        transform_list.append(transforms.Lambda(lambda img: __scale_width(img, opt.loadSize, opt.fineSize, method)))
+    
+    if 'crop' in opt.preprocess:
+        if params is None:
+            transform_list.append(transforms.RandomCrop(opt.fineSize))
+        else:
+            transform_list.append(transforms.Lambda(lambda img: __crop(img, params['crop_pos'], opt.fineSize)))
+
+    if opt.preprocess == 'none':
+        transform_list.append(transforms.Lambda(lambda img: __make_power_2(img, base=4, method=method)))
+
+    
+    if opt.isTrain and not opt.no_flip:
+        if params is None:
+            transform_list.append(transforms.RandomHorizontalFlip())
+        elif params['flip']:
+            transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
+
+    if convert:
+        transform_list += [transforms.ToTensor()]
+        if grayscale:
+            transform_list += [transforms.Normalize((0.5,), (0.5,))]
+        else:
+            transform_list += [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+
     return transforms.Compose(transform_list)
 
 def __scale_width(img, target_width):
