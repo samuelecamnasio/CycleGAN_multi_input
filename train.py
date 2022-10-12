5ae15ea41ccb1b97c92e8b1dc8b6bb89a5618177
Samuele Camnasio <samuele.camnasio@mail.polimi.it>
2022-10-12 17:44:09 +0200

    update train.py

--- train.py
+++ train.py
@@ -17,28 +17,62 @@ total_steps = 0
 for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
     epoch_start_time = time.time()
     epoch_iter = 0
-
+    WIPTest=[]
+    one=0
+    two=0
+    three=0
+    four=0
     for i, data in enumerate(dataset):
+        if opt.no_input==2:
+            if(data['A1_paths']==['./datasets/MRI2CT_inOutJoin/trainA_in/5IMG-0016-00052.png'] and one==0):
+                one=1
+                print('found')
+                WIPTest.append(data)
+            if(data['A1_paths']==['./datasets/MRI2CT_inOutJoin/trainA_in/1IMG-0004-00032.png'] and two==0):
+                two=1
+                WIPTest.append(data)
+            if(data['A1_paths']==['./datasets/MRI2CT_inOutJoin/trainA_in/22IMG-0005-00044.png'] and three==0):
+                three=1
+                WIPTest.append(data)
+            if(data['A1_paths']==['./datasets/MRI2CT_inOutJoin/trainA_in/34IMG-0005-00060.png'] and four==0):
+                four=1
+                WIPTest.append(data)
+        else:
+            if(data['A1_paths']==['./datasets/MRI2CT_inOutJoin/trainA_in/5IMG-0016-00052.png'] and one==0):
+                one=1
+                print('found')
+                WIPTest.append(data)
+            if(data['A1_paths']==['./datasets/MRI2CT_inOutJoin/trainA_inT2/1IMG-0004-00032.png'] and two==0):
+                two=1
+                WIPTest.append(data)
+            if(data['A1_paths']==['./datasets/MRI2CT_inOutJoin/trainA_inT2/22IMG-0005-00044.png'] and three==0):
+                three=1
+                WIPTest.append(data)
+            if(data['A1_paths']==['./datasets/MRI2CT_inOutJoin/trainA_inT2/34IMG-0005-00060.png'] and four==0):
+                four=1
+                WIPTest.append(data)
+        #print(str(data['A1_paths']) + ' ' + str(data['A2_paths']))
         iter_start_time = time.time()
         total_steps += opt.batchSize
         epoch_iter += opt.batchSize
         model.set_input(data)
+        #print(data)
         model.optimize_parameters()
 
-        if total_steps % opt.display_freq == 0:
-            visualizer.display_current_results(model.get_current_visuals(), epoch)
+        #if total_steps % opt.display_freq == 0:
+         #   visualizer.display_current_results(model.get_current_visuals(), epoch)
 
-        if total_steps % opt.print_freq == 0:
-            errors = model.get_current_errors()
-            t = (time.time() - iter_start_time) / opt.batchSize
-            visualizer.print_current_errors(epoch, epoch_iter, errors, t)
-            if opt.display_id > 0:
-                visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
+        #if total_steps % opt.print_freq == 0:
+            #errors = model.get_current_errors()
+            #t = (time.time() - iter_start_time) / opt.batchSize
+            #visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+            #if opt.display_id > 0:
+             #   visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
 
-        if total_steps % opt.save_latest_freq == 0:
-            print('saving the latest model (epoch %d, total_steps %d)' %
-                  (epoch, total_steps))
-            model.save('latest')
+        #if total_steps % opt.save_latest_freq == 0:
+         #   print('saving the latest model (epoch %d, total_steps %d)' %
+           #       (epoch, total_steps))
+            #model.save('latest')
 
     if epoch % opt.save_epoch_freq == 0:
         print('saving the model at the end of epoch %d, iters %d' %
@@ -49,3 +83,11 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
     print('End of epoch %d / %d \t Time Taken: %d sec' %
           (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
     model.update_learning_rate()
+    index=1
+    for data in WIPTest:
+        model.set_input(data)
+        model.test()
+        visualizer.display_current_results(model.get_current_visuals(), epoch, index)
+        index=index+1
+        
+    
