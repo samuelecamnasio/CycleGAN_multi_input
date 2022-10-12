fb0e10ac63dc3a6f5767ad6289cd02d34b8dfa67
Samuele Camnasio <samuele.camnasio@mail.polimi.it>
2022-10-12 17:27:45 +0200

    Update unaligned_dataset.py

--- data/unaligned_dataset.py
+++ data/unaligned_dataset.py
@@ -1,6 +1,6 @@
 import os.path
 import torchvision.transforms as transforms
-from data.base_dataset import BaseDataset, get_transform
+from data.base_dataset import BaseDataset, get_transform_A, get_transform_B
 from data.image_folder import make_dataset
 from PIL import Image
 import PIL
@@ -12,42 +12,71 @@ class UnalignedDataset(BaseDataset):
     def initialize(self, opt):
         self.opt = opt
         self.root = opt.dataroot
-        self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')
+        self.dir_A1 = os.path.join(opt.dataroot, opt.phase + 'A_in')
+        self.dir_A2 = os.path.join(opt.dataroot, opt.phase + 'A_out')
+        if(opt.no_input==3):
+            self.dir_A3 = os.path.join(opt.dataroot, opt.phase + 'A_t2')
+            self.dir_A1 = os.path.join(opt.dataroot, opt.phase + 'A_inT2')
+            self.dir_A2 = os.path.join(opt.dataroot, opt.phase + 'A_outT2')
         self.dir_B = os.path.join(opt.dataroot, opt.phase + 'B')
-	self.no_input = opt.no_input
-        self.A_paths = make_dataset(self.dir_A)
+        self.no_input = opt.no_input
+        self.A1_paths = make_dataset(self.dir_A1)
+        self.A2_paths = make_dataset(self.dir_A2)
+        if(opt.no_input==3):
+            self.A3_paths = make_dataset(self.dir_A3)
         self.B_paths = make_dataset(self.dir_B)
-
-        self.A_paths = sorted(self.A_paths)
+        
+        self.A1_paths = sorted(self.A1_paths)
+        self.A2_paths = sorted(self.A2_paths)
+        if(opt.no_input==3):
+            self.A3_paths = sorted(self.A3_paths)
         self.B_paths = sorted(self.B_paths)
-        self.A_size = len(self.A_paths)
+        print(len(self.A1_paths))
+        print(len(self.A2_paths))
+        if(opt.no_input==3):
+            print(len(self.A3_paths))
+        print(len(self.B_paths))
+        self.A1_size = len(self.A1_paths)
+        self.A2_size = len(self.A2_paths)
+        if(opt.no_input==3):
+            self.A3_size = len(self.A3_paths)
         self.B_size = len(self.B_paths)
-        self.transform = get_transform(opt)
+        ##self.transform = get_transform(opt)
 
         osize = [opt.loadSize, opt.loadSize*self.opt.input_nc]
-	opt.fineSize*self.no_input*self.opt.input_nc
-	self.transformA = []
-        self.transformA.append(transforms.Scale((opt.fineSize*2 , opt.fineSize), Image.BICUBIC))
-	self.transformA += [transforms.ToTensor(),
-                       transforms.Normalize((0.5, 0.5, 0.5),
-                                            (0.5, 0.5, 0.5))]
-        #self.transformA.append(transforms.RandomCrop((opt.fineSize,opt.fineSize*self.opt.input_nc) ))
-	self.transformA = transforms.Compose(self.transformA)
+        opt.fineSize*self.no_input*self.opt.input_nc
+        
+        self.transform_B = get_transform_B(self.opt, grayscale=(self.opt.output_nc == 1))
+        
     def __getitem__(self, index):
-        A_path = self.A_paths[index % self.A_size]
-        index_A = index % self.A_size
+        A1_path = self.A1_paths[index % self.A1_size]
+        A2_path = self.A2_paths[index % self.A2_size]
+        if(self.no_input==3):
+            A3_path = self.A3_paths[index % self.A3_size]
+        index_A1 = index % self.A1_size
+        index_A2 = index % self.A2_size
+        if(self.no_input==3):
+            index_A3 = index % self.A3_size
+        #print('a1 ' + str(self.A1_size))
+        #print('a2 ' + str(self.A2_size))
         index_B = random.randint(0, self.B_size - 1)
         B_path = self.B_paths[index_B]
-	       
-	# print('(A, B) = (%d, %d)' % (index_A, index_B))
-        A_img = Image.open(A_path)#.convert('RGB') # A image is a no_input*3 collection of images
-	A_img = (self.transformA(A_img))
-	A1 = A_img[1,0:256,:]
-	A2 = A_img[1,256:512,:]
-	A1 = A1.unsqueeze(0).numpy()
-	A2 = A2.unsqueeze(0).numpy()
-	B_img = Image.open(B_path)#.convert('RGB')
-        B = self.transform(B_img)
+               
+        # print('(A, B) = (%d, %d)' % (index_A, index_B))
+        A1_img = Image.open(A1_path)#.convert('RGB') # A image is a no_input*3 collection of images
+        A2_img = Image.open(A2_path)#.convert('RGB') # A image is a no_input*3 collection of images
+        if(self.no_input==3):
+            A3_img = Image.open(A3_path)#.convert('RGB') # A image is a no_input*3 collection of images
+        i, j, h, w = transforms.RandomCrop.get_params(A1_img, output_size=(256, 256))
+        flip= random.random()
+        #self.transform_A = get_transform_A(self.opt, flip, i,j,h,w, grayscale=(self.opt.input_nc == 1))
+        A1 = get_transform_A(self.opt,A1_img, flip, i,j,h,w, grayscale=(self.opt.input_nc == 1))
+        A2 = get_transform_A(self.opt,A2_img, flip, i,j,h,w, grayscale=(self.opt.input_nc == 1))      
+        if(self.no_input==3):
+            A3 = get_transform_A(self.opt,A3_img, flip, i,j,h,w, grayscale=(self.opt.input_nc == 1)) 
+        B_img = Image.open(B_path).convert('RGB')
+        B = self.transform_B(B_img)
+        
         if self.opt.which_direction == 'BtoA':
             input_nc = self.opt.output_nc
             output_nc = self.opt.input_nc
@@ -63,11 +92,15 @@ class UnalignedDataset(BaseDataset):
         #if output_nc == 1:  # RGB to gray
         #    tmp = B[0, ...] * 0.299 + B[1, ...] * 0.587 + B[2, ...] * 0.114
         #    B = tmp.unsqueeze(0)
-        return {'A1': A1, 'A2':A2, 'B': B,
-                'A_paths': A_path, 'B_paths': B_path}
+        if(self.no_input==3):
+            return {'A1': A1, 'A2':A2, 'A3':A3, 'B': B,
+                'A1_paths': A1_path, 'A2_paths': A2_path, 'A3_paths': A3_path, 'B_paths': B_path}
+        else:
+            return {'A1': A1, 'A2':A2, 'B': B,
+                'A1_paths': A1_path, 'A2_paths': A2_path, 'B_paths': B_path}
 
     def __len__(self):
-        return max(self.A_size, self.B_size)
+        return max(self.A1_size, self.B_size)
 
     def name(self):
         return 'UnalignedDataset'
